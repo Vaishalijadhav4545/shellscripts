f507c8a99f07969f61979c545135436c5a742ffb
vaishali jadhav <vaishali11025@gmail.com>
2024-04-03 12:57:45 +0530

    this is my commit

--- Script2.sh
+++ Script2.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/
+
+
 #This script is to read input from the user/console
 echo "Value of a"
 read a
 echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
-echo "Welcome to DevOps"
+echo "Welcome to DevOps..."
